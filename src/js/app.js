1ee4d2b0cf710543949b42c64bec32f65d5a82b0
Ilya Krasilnikov <kravus27@gmail.com>
2020-01-08 00:06:19 +0300

    app modified

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 let massive = [
-    {name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
-    {name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
-    {name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
-  ];
+  {name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
+  {name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
+  {name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
+];
 
 function findBy (name, value) {
-  if (Object.name === value) {
-    return true
-  }
+if (name = value) {
+  return true
+}
 } 
 
 let finder = findBy('name','урон');
