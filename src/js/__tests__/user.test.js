a10d7b67ab15d21d42de02087f1306cbb5e96af8
Ilya Krasilnikov <kravus27@gmail.com>
2020-01-13 23:24:18 +0300

    second commit

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -1,16 +1,16 @@
+/* eslint-disable no-undef */
 import findBy from '../app';
 
-let arr = [
-  {name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями'},
-  {name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием'},
-  {name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'},
-]
+const arr = [
+  { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
+  { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
+  { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
+];
 
 test('should return object by name and value', () => {
-  let finder = findBy('name', 'урон');
-  let results = arr.filter(finder);
+  const finder = findBy('name', 'урон');
+  const results = arr.filter(finder);
   const received = results;
-  const expected = [{name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса'}];
+  const expected = [{ name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' }];
   expect(received).toEqual(expected);
-  
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,5 +1,6 @@
 export default function sum(items) {
   let result = 0;
+  // eslint-disable-next-line no-restricted-syntax
   for (const item of items) {
     result += item;
   }
